276f818b73cb3eba3320024617ef4bcd51f66752
Brennan <brennan.slade@selu.edu>
2025-03-17 22:12:21 -0500

    file selection feature and bug fix
    fixed checking metadata of directory instead of only files

--- src/main/java/org/example/keystone/api/MainApplicationController.java
+++ src/main/java/org/example/keystone/api/MainApplicationController.java
@@ -66,8 +66,7 @@ public class MainApplicationController implements Initializable {
     //public TreeTableColumn<MetadataEntry, String> metadataTableValueCol;
 
     public String directoryPath = "src/images/";
-
-    public String fileName;
+    
 
     @FXML
     protected void onHelloButtonClick() {
@@ -214,9 +213,14 @@ public class MainApplicationController implements Initializable {
 
             });
             cell.addEventFilter(MouseEvent.MOUSE_CLICKED, mouseEvent -> {
-                fileName = cell.getText();
 
-                loadMetadata();
+                File cellFile = cell.getItem();
+
+                if (!cellFile.isDirectory()) {
+                    String filePath = cellFile.getAbsolutePath();
+
+                    loadMetadata(filePath);
+                }
 
             });
             return cell;
@@ -232,6 +236,7 @@ public class MainApplicationController implements Initializable {
         if (files != null) {
             for (File file : files) {
                 TreeItem<File> childItem = new TreeItem<>(file);
+
                 parentItem.getChildren().add(childItem);
 
                 if (file.isDirectory()) {
@@ -278,9 +283,9 @@ public class MainApplicationController implements Initializable {
 
     }
 
-    public void loadMetadata() {
+    public void loadMetadata(String filePath) {
 
-        File file = new File(directoryPath + '/' + fileName);
+        File file = new File(filePath);
         MetadataDecoder metadataDecoder = MetadataDecoderFactory.createDecoder(file);
         assert metadataDecoder != null;
         XMLNode xmlRootNode = gdal.ParseXMLString(metadataDecoder.getSpatialReferenceXML());
