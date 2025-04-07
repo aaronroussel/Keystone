3bbab38796dd7501cdd2ec5ff797b525e56b1a42
Brennan <brennan.slade@selu.edu>
2025-04-06 22:49:31 -0500

    changed tabpane to right side

--- src/main/java/org/example/keystone/api/MainApplicationController.java
+++ src/main/java/org/example/keystone/api/MainApplicationController.java
@@ -82,9 +82,16 @@ public class MainApplicationController implements Initializable {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
 
+    // bind width to allow resizing with splitpane
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        imageViewer.fitWidthProperty().bind(imagePreviewAnchorPane.widthProperty());
 
+
+        metadataTable.prefWidthProperty().bind(imagePreviewAnchorPane.widthProperty());
+
+        metadataTableKeyCol.prefWidthProperty().bind(imagePreviewAnchorPane.widthProperty().divide(2));
+        metadataTableValueCol.prefWidthProperty().bind(imagePreviewAnchorPane.widthProperty().divide(2));
     }
 
 
@@ -110,14 +117,15 @@ public class MainApplicationController implements Initializable {
             });
             cell.addEventFilter(MouseEvent.MOUSE_CLICKED, mouseEvent -> {
                 File cellFile = cell.getItem();
-                if (!cellFile.isDirectory()) {
+
+
+
+                if (cellFile != null && !cellFile.isDirectory()) {
                     String filePath = cellFile.getAbsolutePath();
                     try {
                         Image image = ImageFactory.getFXImage(cellFile);
 
                         imageViewer.setImage(image);
-
-                        imageViewer.fitWidthProperty().bind(imagePreviewAnchorPane.widthProperty());
 
                     } catch (Exception e) {
                         System.err.println("Error Loading Image: " + e);
